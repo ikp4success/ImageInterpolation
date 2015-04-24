236982c46ff4a7b8f25c551cc05e972001ff2a37
Immanuel I George <ikp4success@yahoo.com>
2015-04-24 10:16:05 -0400

    ImageInterpolation.java

--- ImageInterpolation.java
+++ ImageInterpolation.java
@@ -2,7 +2,6 @@ package application;
 
 import java.awt.Graphics2D;
 
-
 import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsEnvironment;
 import java.awt.RenderingHints;
@@ -18,7 +17,6 @@ import javax.imageio.ImageWriteParam;
 import javax.imageio.ImageWriter;
 import javax.imageio.stream.FileImageOutputStream;
 
-
 /**
  * Names/Authors: Immanuel I George , Graham Sullivan Class: MTH311 (Numerical
  * Analysis) Final Project - Bi-Cubic Spline Image Interpolation Objective -
@@ -31,187 +29,194 @@ public class ImageInterpolation {
 	public ImageInterpolation() {
 		super();
 	}
-    
-   public String BICublicInterpolation(String imageLoc,int X, int Y) {
-    	
-    	File outFile =null;
-	try {
-//	    Scanner input = new Scanner(System.in);
-//	    System.out.print("Please type a file path: ");
-//	    String filePath = input.next();
-//	    input.close();
-		
-	    BufferedImage source = ImageIO.read(new File(imageLoc));
-	    
-	    // variables that determine what size the new image will be
-//	    int destWidth = 1440;
-//	    int destHeight = 900;
-	    int destWidth = X;
-	    int destHeight = Y;
-	    
-	    // x and y proportions from original to new size
-	    double xProportion = Integer.valueOf(destWidth).doubleValue()
-		    / Integer.valueOf(source.getWidth()).doubleValue();
-	    double yProportion = Integer.valueOf(destHeight).doubleValue()
-		    / Integer.valueOf(source.getHeight()).doubleValue();
-	    
-	    GraphicsConfiguration graphicsConfig = GraphicsEnvironment
-		    .getLocalGraphicsEnvironment().getDefaultScreenDevice()
-		    .getDefaultConfiguration();
-	    BufferedImage buffImage = graphicsConfig.createCompatibleImage(destWidth, destHeight,
-		    source.getColorModel().getTransparency());
-	    
-	    // perform the interpolation
-	    Graphics2D newImage = null;
-	    newImage = buffImage.createGraphics();
-	    newImage.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-		    RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-	    AffineTransform at = AffineTransform.getScaleInstance(xProportion, yProportion);
-	    newImage.drawRenderedImage(source, at);
-	    newImage.dispose();
-	    
-	    // Write the new image to file - need to change this to display in
-	    // gui instead
-	    Iterator iter = ImageIO.getImageWritersByFormatName("JPG");
-	    if (iter.hasNext()) {
-		ImageWriter writer = (ImageWriter) iter.next();
-		ImageWriteParam iwp = writer.getDefaultWriteParam();
-		iwp.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-		iwp.setCompressionQuality(0.95f);
-		
-		outFile = new File("output_1.jpg");
-		FileImageOutputStream output = new FileImageOutputStream(outFile);
-		writer.setOutput(output);
-		IIOImage image = new IIOImage(buffImage, null, null);
-		writer.write(null, image, iwp);
-		output.close();
-	    }
-	} catch (Throwable e) {
-	    e.printStackTrace();
-	}
-	return outFile.getAbsolutePath();
-    }
-   
-   
-   public String NearestNeighbour(String imageLoc,int X, int Y) {
-   	
-   	File outFile =null;
-	try {
-//	    Scanner input = new Scanner(System.in);
-//	    System.out.print("Please type a file path: ");
-//	    String filePath = input.next();
-//	    input.close();
-		
-	    BufferedImage source = ImageIO.read(new File(imageLoc));
-	    
-	    // variables that determine what size the new image will be
-//	    int destWidth = 1440;
-//	    int destHeight = 900;
-	    int destWidth = X;
-	    int destHeight = Y;
-	    
-	    // x and y proportions from original to new size
-	    double xProportion = Integer.valueOf(destWidth).doubleValue()
-		    / Integer.valueOf(source.getWidth()).doubleValue();
-	    double yProportion = Integer.valueOf(destHeight).doubleValue()
-		    / Integer.valueOf(source.getHeight()).doubleValue();
-	    
-	    GraphicsConfiguration graphicsConfig = GraphicsEnvironment
-		    .getLocalGraphicsEnvironment().getDefaultScreenDevice()
-		    .getDefaultConfiguration();
-	    BufferedImage buffImage = graphicsConfig.createCompatibleImage(destWidth, destHeight,
-		    source.getColorModel().getTransparency());
-	    
-	    // perform the interpolation
-	    Graphics2D newImage = null;
-	    newImage = buffImage.createGraphics();
-	    newImage.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-		    RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
-	    AffineTransform at = AffineTransform.getScaleInstance(xProportion, yProportion);
-	    newImage.drawRenderedImage(source, at);
-	    newImage.dispose();
-	    
-	    // Write the new image to file - need to change this to display in
-	    // gui instead
-	    Iterator iter = ImageIO.getImageWritersByFormatName("JPG");
-	    if (iter.hasNext()) {
-		ImageWriter writer = (ImageWriter) iter.next();
-		ImageWriteParam iwp = writer.getDefaultWriteParam();
-		iwp.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-		iwp.setCompressionQuality(0.95f);
-		
-		outFile = new File("output_1.jpg");
-		FileImageOutputStream output = new FileImageOutputStream(outFile);
-		writer.setOutput(output);
-		IIOImage image = new IIOImage(buffImage, null, null);
-		writer.write(null, image, iwp);
-		output.close();
-	    }
-	} catch (Throwable e) {
-	    e.printStackTrace();
-	}
-	return outFile.getAbsolutePath();
-   }
-   
-   
-   public String BILinearInterpolation(String imageLoc,int X, int Y) {
-	   	
-	   	File outFile =null;
+
+	public String BICublicInterpolation(String imageLoc, int X, int Y) {
+
+		File outFile = null;
 		try {
-//		    Scanner input = new Scanner(System.in);
-//		    System.out.print("Please type a file path: ");
-//		    String filePath = input.next();
-//		    input.close();
-			
-		    BufferedImage source = ImageIO.read(new File(imageLoc));
-		    
-		    // variables that determine what size the new image will be
-//		    int destWidth = 1440;
-//		    int destHeight = 900;
-		    int destWidth = X;
-		    int destHeight = Y;
-		    
-		    // x and y proportions from original to new size
-		    double xProportion = Integer.valueOf(destWidth).doubleValue()
-			    / Integer.valueOf(source.getWidth()).doubleValue();
-		    double yProportion = Integer.valueOf(destHeight).doubleValue()
-			    / Integer.valueOf(source.getHeight()).doubleValue();
-		    
-		    GraphicsConfiguration graphicsConfig = GraphicsEnvironment
-			    .getLocalGraphicsEnvironment().getDefaultScreenDevice()
-			    .getDefaultConfiguration();
-		    BufferedImage buffImage = graphicsConfig.createCompatibleImage(destWidth, destHeight,
-			    source.getColorModel().getTransparency());
-		    
-		    // perform the interpolation
-		    Graphics2D newImage = null;
-		    newImage = buffImage.createGraphics();
-		    newImage.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-			    RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-		    AffineTransform at = AffineTransform.getScaleInstance(xProportion, yProportion);
-		    newImage.drawRenderedImage(source, at);
-		    newImage.dispose();
-		    
-		    // Write the new image to file - need to change this to display in
-		    // gui instead
-		    Iterator iter = ImageIO.getImageWritersByFormatName("JPG");
-		    if (iter.hasNext()) {
-			ImageWriter writer = (ImageWriter) iter.next();
-			ImageWriteParam iwp = writer.getDefaultWriteParam();
-			iwp.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-			iwp.setCompressionQuality(0.95f);
-			
-			outFile = new File("output_1.jpg");
-			FileImageOutputStream output = new FileImageOutputStream(outFile);
-			writer.setOutput(output);
-			IIOImage image = new IIOImage(buffImage, null, null);
-			writer.write(null, image, iwp);
-			output.close();
-		    }
+			// Scanner input = new Scanner(System.in);
+			// System.out.print("Please type a file path: ");
+			// String filePath = input.next();
+			// input.close();
+
+			BufferedImage source = ImageIO.read(new File(imageLoc));
+
+			// variables that determine what size the new image will be
+			// int destWidth = 1440;
+			// int destHeight = 900;
+			int destWidth = X;
+			int destHeight = Y;
+
+			// x and y proportions from original to new size
+			double xProportion = Integer.valueOf(destWidth).doubleValue()
+					/ Integer.valueOf(source.getWidth()).doubleValue();
+			double yProportion = Integer.valueOf(destHeight).doubleValue()
+					/ Integer.valueOf(source.getHeight()).doubleValue();
+
+			GraphicsConfiguration graphicsConfig = GraphicsEnvironment
+					.getLocalGraphicsEnvironment().getDefaultScreenDevice()
+					.getDefaultConfiguration();
+			BufferedImage buffImage = graphicsConfig.createCompatibleImage(
+					destWidth, destHeight, source.getColorModel()
+							.getTransparency());
+
+			// perform the interpolation
+			Graphics2D newImage = null;
+			newImage = buffImage.createGraphics();
+			newImage.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+					RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+			AffineTransform at = AffineTransform.getScaleInstance(xProportion,
+					yProportion);
+			newImage.drawRenderedImage(source, at);
+			newImage.dispose();
+
+			// Write the new image to file - need to change this to display in
+			// gui instead
+			Iterator iter = ImageIO.getImageWritersByFormatName("JPG");
+			if (iter.hasNext()) {
+				ImageWriter writer = (ImageWriter) iter.next();
+				ImageWriteParam iwp = writer.getDefaultWriteParam();
+				iwp.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+				iwp.setCompressionQuality(0.95f);
+
+				outFile = new File("output_1.jpg");
+				FileImageOutputStream output = new FileImageOutputStream(
+						outFile);
+				writer.setOutput(output);
+				IIOImage image = new IIOImage(buffImage, null, null);
+				writer.write(null, image, iwp);
+				output.close();
+			}
 		} catch (Throwable e) {
-		    e.printStackTrace();
+			e.printStackTrace();
 		}
 		return outFile.getAbsolutePath();
-	   }
-    
+	}
+
+	public String NearestNeighbour(String imageLoc, int X, int Y) {
+
+		File outFile = null;
+		try {
+			// Scanner input = new Scanner(System.in);
+			// System.out.print("Please type a file path: ");
+			// String filePath = input.next();
+			// input.close();
+
+			BufferedImage source = ImageIO.read(new File(imageLoc));
+
+			// variables that determine what size the new image will be
+			// int destWidth = 1440;
+			// int destHeight = 900;
+			int destWidth = X;
+			int destHeight = Y;
+
+			// x and y proportions from original to new size
+			double xProportion = Integer.valueOf(destWidth).doubleValue()
+					/ Integer.valueOf(source.getWidth()).doubleValue();
+			double yProportion = Integer.valueOf(destHeight).doubleValue()
+					/ Integer.valueOf(source.getHeight()).doubleValue();
+
+			GraphicsConfiguration graphicsConfig = GraphicsEnvironment
+					.getLocalGraphicsEnvironment().getDefaultScreenDevice()
+					.getDefaultConfiguration();
+			BufferedImage buffImage = graphicsConfig.createCompatibleImage(
+					destWidth, destHeight, source.getColorModel()
+							.getTransparency());
+
+			// perform the interpolation
+			Graphics2D newImage = null;
+			newImage = buffImage.createGraphics();
+			newImage.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+					RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+			AffineTransform at = AffineTransform.getScaleInstance(xProportion,
+					yProportion);
+			newImage.drawRenderedImage(source, at);
+			newImage.dispose();
+
+			// Write the new image to file - need to change this to display in
+			// gui instead
+			Iterator iter = ImageIO.getImageWritersByFormatName("JPG");
+			if (iter.hasNext()) {
+				ImageWriter writer = (ImageWriter) iter.next();
+				ImageWriteParam iwp = writer.getDefaultWriteParam();
+				iwp.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+				iwp.setCompressionQuality(0.95f);
+
+				outFile = new File("output_1.jpg");
+				FileImageOutputStream output = new FileImageOutputStream(
+						outFile);
+				writer.setOutput(output);
+				IIOImage image = new IIOImage(buffImage, null, null);
+				writer.write(null, image, iwp);
+				output.close();
+			}
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		return outFile.getAbsolutePath();
+	}
+
+	public String BILinearInterpolation(String imageLoc, int X, int Y) {
+
+		File outFile = null;
+		try {
+			// Scanner input = new Scanner(System.in);
+			// System.out.print("Please type a file path: ");
+			// String filePath = input.next();
+			// input.close();
+
+			BufferedImage source = ImageIO.read(new File(imageLoc));
+
+			// variables that determine what size the new image will be
+			// int destWidth = 1440;
+			// int destHeight = 900;
+			int destWidth = X;
+			int destHeight = Y;
+
+			// x and y proportions from original to new size
+			double xProportion = Integer.valueOf(destWidth).doubleValue()
+					/ Integer.valueOf(source.getWidth()).doubleValue();
+			double yProportion = Integer.valueOf(destHeight).doubleValue()
+					/ Integer.valueOf(source.getHeight()).doubleValue();
+
+			GraphicsConfiguration graphicsConfig = GraphicsEnvironment
+					.getLocalGraphicsEnvironment().getDefaultScreenDevice()
+					.getDefaultConfiguration();
+			BufferedImage buffImage = graphicsConfig.createCompatibleImage(
+					destWidth, destHeight, source.getColorModel()
+							.getTransparency());
+
+			// perform the interpolation
+			Graphics2D newImage = null;
+			newImage = buffImage.createGraphics();
+			newImage.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+					RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+			AffineTransform at = AffineTransform.getScaleInstance(xProportion,
+					yProportion);
+			newImage.drawRenderedImage(source, at);
+			newImage.dispose();
+
+			// Write the new image to file - need to change this to display in
+			// gui instead
+			Iterator iter = ImageIO.getImageWritersByFormatName("JPG");
+			if (iter.hasNext()) {
+				ImageWriter writer = (ImageWriter) iter.next();
+				ImageWriteParam iwp = writer.getDefaultWriteParam();
+				iwp.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+				iwp.setCompressionQuality(0.95f);
+
+				outFile = new File("output_1.jpg");
+				FileImageOutputStream output = new FileImageOutputStream(
+						outFile);
+				writer.setOutput(output);
+				IIOImage image = new IIOImage(buffImage, null, null);
+				writer.write(null, image, iwp);
+				output.close();
+			}
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		return outFile.getAbsolutePath();
+	}
+
 }
